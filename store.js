fcdb0bc318bafa7da5f445947d27a8071f42c584
Berto Yáñez <berto@ber.to>
2019-01-22 10:50:11 +0100

    Name redux instance

--- store.js
+++ store.js
@@ -3,10 +3,14 @@ import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers';
 
+const composeEnhancers = composeWithDevTools({
+  name: 'Insurgent Composer'
+});
+
 export const initStore = (initialState = {}) => {
   return createStore(
     reducer,
     initialState,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
+    composeEnhancers(applyMiddleware(thunkMiddleware))
   );
 };
